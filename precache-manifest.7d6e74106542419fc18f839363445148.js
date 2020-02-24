e5eb534e2945c8a151cfd83dfac6c4e53e42bb81
Kzero <tennoken@naver.com>
2020-02-24 13:54:33 +0900

    Updates

---
+++ precache-manifest.7d6e74106542419fc18f839363445148.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd72669ccad6020377541be0cde2d5c",
+    "url": "/todolist-weather-react-app/index.html"
+  },
+  {
+    "revision": "8c1ed97bff53360e3dfb",
+    "url": "/todolist-weather-react-app/static/js/2.149b16a9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/todolist-weather-react-app/static/js/2.149b16a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cb8c05f1decf6d6bb01",
+    "url": "/todolist-weather-react-app/static/js/main.d2cc89ec.chunk.js"
+  },
+  {
+    "revision": "b7a0c6e162c41e96b870",
+    "url": "/todolist-weather-react-app/static/js/runtime-main.f203e8d6.js"
+  }
+]);
